5b184a8c3558c3557cbf242daebf6f2885330760
Erik Uunila <erik.uunila@churchmedia.com>
2016-07-13 12:54:38 -0500

    Fixed 'this' issue

--- fade-in-out.js
+++ fade-in-out.js
@@ -3,18 +3,21 @@
  */
 ;
 (function( $ ) {
-  $.fn.fadeInOut = function(element) {
+  $.fn.fadeInOut = function() {
+    var element = this;
     var offset = $(window).scrollTop() / 250;
-    function opacityFade(offset) {
+    if (offset <= 1) {
+      element.css('opacity', 0 + $(window).scrollTop() / 250);
+    } else {
+      element.css('opacity', 1);
+    }
+    $(window).scroll(function(){
+      offset = $(window).scrollTop() / 250;
       if (offset <= 1) {
         element.css('opacity', 0 + $(window).scrollTop() / 250);
       } else {
         element.css('opacity', 1);
       }
-    }
-    $(window).scroll(function(){
-      offset = $(window).scrollTop() / 250;
-      opacityFade(offset);
     });
   };
 })(jQuery);
